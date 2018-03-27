7eac6d6f1ec27108440248bd3a29a1a75c7cd8ab
lindongwu <lindongwu@memect.co>
2018-03-28 01:29:22 +0800

    feature: user file.get

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,12 +1,13 @@
-/* global self, Response */
-const IPFS = importScripts('https://cdn.jsdelivr.net/npm/ipfs/dist/index.js');
+/* global self, Response, Ipfs */
+// inject Ipfs to global
+importScripts('https://cdn.jsdelivr.net/npm/ipfs/dist/index.js');
 
 let ipfsNode = null;
 
 /** create an in memory node (side effect) */
 function initIPFS() {
   const repoPath = `ipfs-${Math.random()}`;
-  ipfsNode = new IPFS({
+  ipfsNode = new Ipfs({
     repo: repoPath,
     config: {
       Addresses: {
@@ -39,9 +40,10 @@ function getReadyNode() {
 
 /** this will get an array of file, type definition can be found at
  https://github.com/linonetwo/pants-control/commit/9d2e44319bb4932dfd1f29bb5f168011b3407de4#diff-f2c5be8b23901c9c4d8cbe549156bb7fR6 */
-function getFile(hash) {
+async function getFile(hash) {
+  const node = await getReadyNode();
   return new Promise((resolve, reject) => {
-    ipfsNode.files.get(hash, (err, files) => {
+    node.files.get(hash, (err, files) => {
       if (err) {
         return reject(err);
       }
@@ -53,7 +55,8 @@ function getFile(hash) {
 }
 
 /** Using files.cat, see https://github.com/ipfs/ipfs-service-worker
- * This will not deal with folder.
+ * This can not deal with the folder: "Error: this dag node is a directory"
+ * And it will block the page.
  */
 async function catAndRespond(hash) {
   const node = await getReadyNode();
@@ -70,12 +73,16 @@ self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', async event => {
   console.log(`Service worker getting ${event.request.url}`);
-  if (event.request.url.startsWith(`${self.location.origin}/hash`)) {
-    const multihash = event.request.url.split('/hash/')[1];
+  if (event.request.url.startsWith(`${self.location.origin}/ipfs`)) {
+    const multihash = event.request.url.split('/ipfs/')[1];
     console.log(`The hash is ${multihash}`);
 
-    event.respondWith(catAndRespond(multihash));
+    try {
+      const files = await getFile(multihash);
+    } catch (error) {
+      console.error(error);
+    }
   }
 });
