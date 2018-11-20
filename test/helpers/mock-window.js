308319c3b17576b8f1eee6447dd7b555c907df7f
Vasco Santos <vasco.santos@ua.pt>
2018-11-20 12:28:16 +0000

    chore: upgrade dependencies (#31)

--- test/helpers/mock-window.js
+++ test/helpers/mock-window.js
@@ -3,6 +3,7 @@
 module.exports = function mockWindow () {
   const win = {
     listeners: [],
+    Date: Date,
     addEventListener: (_, listener) => win.listeners.push(listener),
     removeEventListener (_, listener) {
       win.listeners = win.listeners.filter(l => l !== listener)
